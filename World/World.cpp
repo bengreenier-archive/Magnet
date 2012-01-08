78358542bd85fb0f9d46fbb7519b333b347cbedf
Ben Greenier <mygithub@b3nhub.com>
2012-01-07 21:01:47 -0500

    begining setup for B2D physics

--- World/World.cpp
+++ World/World.cpp
@@ -3,4 +3,23 @@
 World::World()
 {
     //ctor
+
+    m_world1 = new b2World(b2Vec2(0.0f,10.0f));
+
+}
+
+b2World* World::Access()
+{
+    return this->m_world1;
+}
+
+ sf::RenderWindow* World::RenderAccess()
+ {
+     return Renderer::GetRenderWindow();
+ }
+
+sf::Color World::B2SFColor(const b2Color &color, int alpha)
+{
+	sf::Color result((sf::Uint8)(color.r*255), (sf::Uint8)(color.g*255), (sf::Uint8)(color.b*255), (sf::Uint8) alpha);
+	return result;
 }
--- World/World.h
+++ World/World.h
@@ -1,13 +1,25 @@
 #ifndef WORLD_H
 #define WORLD_H
 
+#include <BOX2D/Box2D.h>
+#include <SFML/Graphics.hpp>
+#include <list>
+
+#include "../Game/Renderer.h"
 
 class World
 {
     public:
         World();
+        b2World* Access(); //World::access()->  to work with. looks nice.
+        sf::RenderWindow* RenderAccess();//just calls renderer cmd, but is "pretty" to have here.
+
+        sf::Color B2SFColor(const b2Color &color, int alpha = 255); //convert a b2color to sfml
+
     protected:
     private:
+        b2World* m_world1; //our first/potentially-only physics world.
+        std::list <b2Body*> b2PhysicsObjects; //a list of all b2 pointers. push_front this.
 };
 
 #endif // WORLD_H
