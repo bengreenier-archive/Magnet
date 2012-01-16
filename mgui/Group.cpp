fa6e4ee919456701aefea9747b9954a62b82e5ac
Adam Zielinski <raptorivaz@gmail.com>
2012-01-15 23:21:32 -0500

    Working on mgui

--- mgui/Group.cpp
+++ mgui/Group.cpp
@@ -29,6 +29,7 @@ void Group::SetPosition(sf::Vector2f newPosition){
 void Group::AddComponent(Component* newCmp){
     if(ComponentExists(newCmp)) return;
 
+    newCmp->SetGroup(this);
     m_component_map.insert(std::pair<const char*, Component*>(newCmp->GetName(), newCmp));
 }
 
@@ -36,8 +37,23 @@ bool Group::ComponentExists(Component* checkcmp){
     return (bool)m_component_map.count(checkcmp->GetName());
 }
 
-void Group::onEvent(sf::Event evt){
 
+
+void Group::onEvent(sf::Event evt){
+    component_map_iterator_type     cmp_map_it;
+
+    switch(evt.Type){
+        case sf::Event::MouseButtonReleased:
+            cmp_map_it = m_component_map.begin();
+            while(cmp_map_it != m_component_map.end()){
+                if(cmp_map_it->second->CheckBounds(sf::Vector2f(evt.MouseButton.X, evt.MouseButton.Y))){
+                    cmp_map_it->second->onClick();
+                }
+
+                cmp_map_it++;
+            }
+            break;
+    }
 }
 
 const char* Group::GetName(){ return m_name; }
