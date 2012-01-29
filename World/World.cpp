eb1cc282dca62fa84751c88e1b4ef828b431e0aa
Ben Greenier <mygithub@b3nhub.com>
2012-01-28 19:02:50 -0500

    WorldManager

--- World/World.cpp
+++ World/World.cpp
@@ -194,14 +194,6 @@ void World::ProcessQueue(std::vector<PhysShape*>* Q,std::string fx)
 }
 
 
-void World::HookHelper()
-{
-    Access()->Step();
-}
-
-void World::AddShape(PhysShape* shape){
-    World::Access()->Queue.push_back(shape);
-}
 
 void World::SetTimestep(float in)
 {
